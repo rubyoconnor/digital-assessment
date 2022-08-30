da5f70f8903867a96a1d2f6d4c51ae35f2323824
Ruby O'Connor <rubyoconnor@Rubys-MacBook-Pro-2.local>
2022-08-31 08:59:36 +1200

    Sprint 4

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def get_string(m):
-    my_string=input(m)
+    my_string = input(m)
     return my_string
 
 
@@ -7,10 +7,10 @@ def get_integer(m):
     my_integer = int(input(m))
     return my_integer
 
-def add_to_order(L,order_list):
+def add_to_order(L, order_list):
     print("_" * 50)
     for i in range(0, len(L)):
-        output = "{} {} ${}".format(i, L[i][0], L[i][1])
+        output = "{} : {} ${}".format(i, L[i][0], L[i][1])
         print(output)
     print("_" * 50)
 
@@ -20,20 +20,52 @@ def add_to_order(L,order_list):
     output = "You have added {} {} to your order".format(quantity, L[which_pasta][0])
     print(output)
 
-    order_list.append([quantity, L[which_pasta][0],  L[which_pasta][1]])
+    order_list.append([quantity, L[which_pasta][0], L[which_pasta][1]])
 
-def review_order(order_list):
-    print("_"*50)
+
+def delete_from_order(order_list):
+    print("_" * 50)
+    for i in range(0, len(order_list)):
+        output = "{} : {} x {}".format(i, order_list[i][0], order_list[i][1])
+        print(output)
+        print("_" * 50)
+
+    which_index = get_integer("Which number pasta would you like to remove from?")
+    quantity = get_integer("How many would you like to remove?")
+    order_list[which_index][0] -= quantity
+
+    if order_list[which_index][0] <= 0:
+        output = "You have removed {} x {} from your order".format(quantity, order_list[which_index][1])
+        print(output)
+        order_list.pop(which_index)
+
+    print("_" * 50)
+    print("This is your updated order")
     total = 0
     for x in order_list:
-        subtotal = x[0]*x[2]
+        subtotal = x[0] * x[2]
         total += subtotal
-        output = "{} x {:<30} at ${:.2f}".format(x[0], x[1],subtotal)
+        output = "{} x {:<30} at ${:.2f}".format(x[0], x[1], subtotal)
         print(output)
     output = "Total = ${:.2f}".format(total)
     print(output)
     print("_" * 50)
     return None
+
+
+def review_order(order_list):
+    print("_" * 50)
+    total = 0
+    for x in order_list:
+        subtotal = x[0] * x[2]
+        total += subtotal
+        output = "{} x {:<30} at ${:.2f}".format(x[0], x[1], subtotal)
+        print(output)
+    output = "Total = ${:.2f}".format(total)
+    print(output)
+    print("_" * 50)
+    return None
+
 
 def print_pasta(L):
     print("_" * 50)
@@ -41,6 +73,7 @@ def print_pasta(L):
         output = "{} ${}".format(x[0], x[1])
         print(output)
     print("_" * 50)
+
 
 def main():
     pasta_list = [
@@ -53,17 +86,17 @@ def main():
         ["Pappardelle Ricci D’Angello", 26],
         ["Raviolo di Salsiccia", 22],
         ["Ravioli di Ricotta", 20]
-        ]
+    ]
 
     menu_list = [
         ["p", "Print Pasta"],
         ["a", "Add Pasta"],
         ["r", "Review Order"],
+        ["d", "Delete from Order"],
         ["q", "Quit"]]
 
     order_list = []
-
-
+    order_list = [[4, "Ravioli di Ricotta", 20], [3, "Spaghetti Pomodoro", 16]]
 
     run_program = True
     while run_program:
@@ -77,11 +110,19 @@ def main():
             add_to_order(pasta_list, order_list)
         elif user_input == "r":
             review_order(order_list)
+        elif user_input == "d":
+            delete_from_order(order_list)
         elif user_input == "q":
             run_program = False
             print("You have quit")
 
+
 main()
+
+
+
+
+
 
 
 
