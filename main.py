9c2e3d1bc729fd50654be8f90a106af7cb0300db
Ruby O'Connor <rubyoconnor@Rubys-MacBook-Pro-2.local>
2022-08-30 19:23:41 +1200

    Sprint 3

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ def get_integer(m):
     my_integer = int(input(m))
     return my_integer
 
-def add_to_order(L):
+def add_to_order(L,order_list):
     print("_" * 50)
     for i in range(0, len(L)):
-        output = "{} {} {}".format(i, L[i][0], L[i][1])
+        output = "{} {} ${}".format(i, L[i][0], L[i][1])
         print(output)
     print("_" * 50)
 
@@ -20,31 +20,50 @@ def add_to_order(L):
     output = "You have added {} {} to your order".format(quantity, L[which_pasta][0])
     print(output)
 
+    order_list.append([quantity, L[which_pasta][0],  L[which_pasta][1]])
+
+def review_order(order_list):
+    print("_"*50)
+    total = 0
+    for x in order_list:
+        subtotal = x[0]*x[2]
+        total += subtotal
+        output = "{} x {:<30} at ${:.2f}".format(x[0], x[1],subtotal)
+        print(output)
+    output = "Total = ${:.2f}".format(total)
+    print(output)
+    print("_" * 50)
+    return None
 
 def print_pasta(L):
     print("_" * 50)
     for x in L:
-        output = "{} {}".format(x[0], x[1])
+        output = "{} ${}".format(x[0], x[1])
         print(output)
     print("_" * 50)
 
 def main():
     pasta_list = [
-        ["Linguine Gamberi", "$23"],
-        ["Fusilli Pesto", "$19"],
-        ["Conchilglie alla Bolognese", "$22"],
-        ["Rigatoni alla Caponata", "$21"],
-        ["Fettuccine Carbonara", "$20"],
-        ["Spaghetti Pomodoro", "$16"],
-        ["Pappardelle Ricci D’Angello", "$26"],
-        ["Raviolo di Salsiccia", "$22"],
-        ["Ravioli di Ricotta", "$20"]
+        ["Linguine Gamberi", 23],
+        ["Fusilli Pesto", 19],
+        ["Conchilglie alla Bolognese", 22],
+        ["Rigatoni alla Caponata", 21],
+        ["Fettuccine Carbonara", 20],
+        ["Spaghetti Pomodoro", 16],
+        ["Pappardelle Ricci D’Angello", 26],
+        ["Raviolo di Salsiccia", 22],
+        ["Ravioli di Ricotta", 20]
         ]
 
     menu_list = [
         ["p", "Print Pasta"],
         ["a", "Add Pasta"],
+        ["r", "Review Order"],
         ["q", "Quit"]]
+
+    order_list = []
+
+
 
     run_program = True
     while run_program:
@@ -55,9 +74,15 @@ def main():
         if user_input == "p":
             print_pasta(pasta_list)
         elif user_input == "a":
-            add_to_order(pasta_list)
+            add_to_order(pasta_list, order_list)
+        elif user_input == "r":
+            review_order(order_list)
         elif user_input == "q":
             run_program = False
             print("You have quit")
 
 main()
+
+
+
+
